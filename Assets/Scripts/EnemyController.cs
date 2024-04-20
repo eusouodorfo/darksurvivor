4428fa9d92eeef37c08deb986235bf4dbf9588f9
Rodolfo "Dorfo" N. Silva <rodolfo.dorfo@outlook.com>
2024-04-19 21:35:17 -0300

    funcionando dano de arma

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,6 +10,7 @@ public class EnemyController : MonoBehaviour
     public float damage;
     public float hitWaitTime = 1f;
     public float hitCounter;
+    public float health = 5f;
 
     void Start()
     {
@@ -22,7 +23,7 @@ public class EnemyController : MonoBehaviour
     {
         theRB.velocity = (target.position - transform.position).normalized * moveSpeed;
 
-        if(hitCounter > 0f)
+        if (hitCounter > 0f)
         {
             hitCounter -= Time.deltaTime;
         }
@@ -30,11 +31,21 @@ public class EnemyController : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player" && hitCounter <= 0f)
+        if (collision.gameObject.tag == "Player" && hitCounter <= 0f)
         {
             PlayerHealthController.instance.TakeDamage(damage);
 
             hitCounter = hitWaitTime;
+        }
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        health -= damageToTake;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/Weapons/SpinWeapon.cs
+++ Assets/Scripts/Weapons/SpinWeapon.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class SpinWeapon : MonoBehaviour
 {
-    
+    public float rotateSpeed;
+    public Transform holder;
+
     void Start()
     {
-        
+
     }
 
-   
+
     void Update()
     {
-        
+        holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotateSpeed * Time.deltaTime));
     }
 }
