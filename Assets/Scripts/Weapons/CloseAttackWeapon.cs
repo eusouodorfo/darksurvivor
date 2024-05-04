e6fe845594863d83d919049344f539fe400625b8
Rodolfo "Dorfo" N. Silva <rodolfo.dorfo@outlook.com>
2024-05-04 07:54:24 -0300

    espadas funcionais

--- Assets/Scripts/Weapons/CloseAttackWeapon.cs
+++ Assets/Scripts/Weapons/CloseAttackWeapon.cs
@@ -10,11 +10,54 @@ public class CloseAttackWeapon : Weapon
 
     void Start()
     {
-
+        SetStats();
     }
 
     void Update()
     {
+        if (statsUpdated == true)
+        {
+            statsUpdated = false;
+            SetStats();
+        }
 
+        attackCounter -= Time.deltaTime;
+
+        if (attackCounter <= 0)
+        {
+            attackCounter = stats[weaponLevel].timeBetweenAttacks;
+
+            direction = Input.GetAxisRaw("Horizontal");
+
+            if (direction != 0)
+            {
+                if (direction > 0)
+                {
+                    damager.transform.rotation = Quaternion.identity;
+                }
+                else
+                {
+                    damager.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+                }
+            }
+
+            Instantiate(damager, damager.transform.position, damager.transform.rotation, transform).gameObject.SetActive(true);
+
+            for (int i = 1; i < stats[weaponLevel].amount; i++)
+            {
+                float rot = (360f / stats[weaponLevel].amount) * i;
+
+                Instantiate(damager, damager.transform.position, Quaternion.Euler(0f, 0f, damager.transform.rotation.eulerAngles.z + rot), transform).gameObject.SetActive(true);
+            }
+
+        }
+    }
+
+    void SetStats()
+    {
+        damager.damageAmount = stats[weaponLevel].damage;
+        damager.lifeTime = stats[weaponLevel].duration;
+        damager.transform.localScale = Vector3.one * stats[weaponLevel].range;
+        attackCounter = 0f;
     }
 }
